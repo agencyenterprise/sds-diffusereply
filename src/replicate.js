5e045e523f9d8263098f51512edd3e78661a28d2
ianribeiroae <ian.ribeiro@ae.studio>
2022-09-19 05:44:54 -0300

    ✨ moved replicate service to external API

--- src/replicate.js
+++ src/replicate.js
@@ -1,52 +1,24 @@
 import * as dotenv from "dotenv";
+import axios from "axios";
+
+import { logError } from "./util.js";
+
 dotenv.config();
 
-import Replicate from "replicate-js";
-
-const ReplicateClient = new Replicate();
-
-const {
-  TXT2IMG_SETTING_HEIGHT,
-  TXT2IMG_SETTING_WIDTH,
-  TXT2IMG_SETTING_GUIDANCE,
-  TXT2IMG_SETTING_STEPS,
-  TXT2IMG_SETTINGS_PROMPTSTR,
-  TXT2IMG_SETTING_OUTPUTS,
-  TXT2IMG_PROMPT_PRE,
-  TXT2IMG_PROMPT_POST
-} = process.env;
-
-const modifier_list = [
-  "fantasy, matte painting, concept art",
-  "scifi, bladerunner, concept art",
-  "ghibli, matte painting, concept art"
-];
-
-const getRandomModifier = () =>
-  modifier_list[Math.floor(Math.random() * modifier_list.length)];
-
-const processPrompt = prompt =>
-  `${
-    TXT2IMG_PROMPT_PRE ? `${TXT2IMG_PROMPT_PRE}, ` : ""
-  }${prompt}, ${getRandomModifier()}${
-    TXT2IMG_PROMPT_POST ? `, ${TXT2IMG_PROMPT_POST}` : ""
-  }`;
+const { TXT2IMG_API_URL } = process.env;
 
 export const callTextToImageReplicate = async (prompt_text, init_image) => {
-  const stableDiffusionModel = await ReplicateClient.models.get(
-    "stability-ai/stable-diffusion"
-  );
-
-  const stableDiffusionImage = await stableDiffusionModel.predict({
-    prompt: processPrompt(prompt_text),
-    num_outputs: TXT2IMG_SETTING_OUTPUTS,
-    width: TXT2IMG_SETTING_WIDTH,
-    height: TXT2IMG_SETTING_HEIGHT,
-    prompt_strength: TXT2IMG_SETTINGS_PROMPTSTR,
-    num_inference_steps: TXT2IMG_SETTING_STEPS,
-    guidance_scale: TXT2IMG_SETTING_GUIDANCE,
-
-    ...(init_image && { init_image })
-  });
-  return stableDiffusionImage;
+  try {
+    const urlSearchParams = new URLSearchParams({
+      preset: init_image ? "single-high-image" : "single-high",
+      prompt: prompt_text,
+      ...(init_image && { init_image })
+    });
+    const response = await axios.get(
+      `${TXT2IMG_API_URL}/queue/sdgen?${urlSearchParams}`
+    );
+    return response.data;
+  } catch (e) {
+    logError(e.message);
+  }
 };
